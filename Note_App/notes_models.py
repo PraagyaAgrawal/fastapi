80bc8511ad8f138046a4c4ec10d23687684144cf
PraagyaAgrawal <praagyaagrawal020@gmail.com>
2025-06-11 14:24:24 +0530

    Update notes_models.py

--- Note_App/notes_models.py
+++ Note_App/notes_models.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, func
 from .notes_db import Base
 
 class Pad(Base):
     __tablename__ = "pads"
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, unique=True, index=True)
+    title = Column(String, index=True)
     content = Column(String)
-
-    def __repr__(self):
-        return f"<Pad(id={self.id}, title={self.title})>"
+    is_active = Column(Boolean, default = True)
+    created_at = Column(DateTime, default = func.now())
+    updated_at = Column(DateTime, default = func.now(), onupdate = func.now())
