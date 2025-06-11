860d7a07e31a24d6fcefa86a5251530501433644
PraagyaAgrawal <praagyaagrawal020@gmail.com>
2025-06-11 14:23:58 +0530

    Update notes_db.py

--- Note_App/notes_db.py
+++ Note_App/notes_db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 url = "sqlite:///./notes.db"
-engine = create_engine(url, connect_args={"check_same_thread":False})
+engine = create_engine(url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
